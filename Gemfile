26d38e7fb7b35a6ce36577153914d0ad020bb962
Bondarenko Marina <marina.gurbenko@gmail.com>
2015-04-25 15:24:43 +0300

    Init app

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+ruby '2.2.2'
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2'
+# Use postgresql as the database for Active Record
+gem 'pg'
+
+gem 'rails-api'
+
+gem 'spring', :group => :development
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails'
+  gem 'byebug'
+end
+
+group :test do
+  gem "shoulda-callback-matchers", "~> 1.0"
+  gem 'faker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Traces::Application.config.secret_token = 'ab80fcecbed5314f91d750956fe41828e684432638f5072d846b46c5e18983b5c2311095657466fcd3e22005e69df2ada64f6cce96f3c10c0bd8b7f351dac7dd'
